51a753f862a0faa6ee71d0d0b05a2be2ded63827
Arthur Murillo <arthurmurillo9@gmail.com>
2022-03-31 08:53:56 +0200

    Update script + login to docker hub

--- run-app.sh
+++ run-app.sh
@@ -4,13 +4,10 @@
 ./mvnw clean && ./mvnw package
 
 # create image
-docker build -t lo-poti-chat:latest .
+docker build -t carduin/lo-poti-chat:latest .
 
-#remove previous container (true prevents error from crashing process if container does not exist)
-docker stop lo-poti-chat || true && docker rm lo-poti-chat || true
-
-#create new container
-docker run -d -p 80:8080 --name lo-poti-chat lo-poti-chat
+#create new container and delete previous if exists
+docker run --rm -d -p 80:8080 --name lo-poti-chat carduin/lo-poti-chat
 
 #cleanup of the old image
 docker rmi `docker images --filter dangling=true -q`
